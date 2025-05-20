6b6e6275dfe375993dfad2aff004647e5d8cb455
Ali Zaghloul <alizaghloul64@gmail.com>
2025-05-20 13:56:45 +0300

    feat(auth): add getUserFromToken function for user retrieval
    - Introduced a new function `getUserFromToken` to extract user
      information from a JWT token, enhancing user authentication flow.
    - Updated the `auth.service.ts` to include `getUserById` and
      `verifyToken` imports for improved user data handling.
    - Ensured proper error handling for cases where the user is not
      found, returning a 404 status.

--- src/db/auth/auth.service.ts
+++ src/db/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { CreateUserInput, LoginUserInput } from '@/schemas/user.schema';
 import { prisma } from '@/lib/prisma';
 import bcrypt from 'bcrypt';
-import { getUserByEmail } from '../user/user.repository';
+import { getUserByEmail, getUserById } from '../user/user.repository';
 import type { AuthenticatedUserDTO, UserDTO } from '@/types/user.types';
-import { generateToken } from '@/lib/jwts';
+import { generateToken, verifyToken } from '@/lib/jwts';
 import { HttpError, HttpStatus } from '@/lib/errors';
+import { User } from '@/generated/prisma';
 
 export async function registerUser(data: CreateUserInput): Promise<UserDTO> {
   if (await getUserByEmail(data.email)) {
@@ -48,4 +49,11 @@ export async function authenticateUser(
     token,
     expiresIn: expiresIn,
   };
+}
+
+export async function getUserFromToken(token: string): Promise<User> {
+  const { userId } = verifyToken(token);
+  const user = await getUserById(BigInt(userId));
+  if (!user) throw new HttpError('User not found', HttpStatus.NOT_FOUND);
+  return user;
 }
--- src/db/user/user.repository.ts
+++ src/db/user/user.repository.ts
@@ -2,3 +2,6 @@ import { prisma } from '@/lib/prisma';
 
 export const getUserByEmail = (email: string) =>
   prisma.user.findUnique({ where: { email } });
+
+export const getUserById = (id: bigint) =>
+  prisma.user.findUnique({ where: { id } });
