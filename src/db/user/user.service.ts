098d697a2b9a635b8345519737451c4722c161b0
Ali Zaghloul <alizaghloul64@gmail.com>
2025-05-20 14:16:47 +0300

    feat(user): add updateUser function and schema validation
    - Implemented `updateUser` function to handle user updates,
      including error handling for non-existent users.
    - Added `updateUserSchema` for input validation using Zod,
      ensuring username, password, and profile picture URL meet
      specified criteria.
    - Updated user types to include `UpdateUserInput` for better
      type management.

--- src/db/user/user.service.ts
+++ src/db/user/user.service.ts
@@ -1,1 +1,30 @@
+import { prisma } from '@/lib/prisma';
+import { UpdateUserInput } from '@/schemas/user.schema';
+import { UserDTO } from '@/types/user.types';
+import { HttpError, HttpStatus } from '@/lib/errors';
+import { getUserById } from './user.repository';
 
+export async function updateUser(
+  userId: string,
+  update: UpdateUserInput
+): Promise<UserDTO> {
+  const existingUser = await getUserById(BigInt(userId));
+
+  if (!existingUser) {
+    throw new HttpError(
+      `User with id ${userId} not found`,
+      HttpStatus.NOT_FOUND
+    );
+  }
+
+  const user = await prisma.user.update({
+    where: { id: BigInt(userId) },
+    data: { ...update },
+  });
+
+  return {
+    id: user.id.toString(),
+    username: user.username,
+    email: user.email,
+  };
+}
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -16,5 +16,19 @@ export const loginUserSchema = z.object({
   password: z.string().min(8),
 });
 
+export const updateUserSchema = z
+  .object({
+    username: z
+      .string()
+      .min(3, { message: 'Username must be atleast 3 characters' })
+      .max(16, { message: 'Username must bt at most 16 characters' }),
+    password: z
+      .string()
+      .min(8, { message: 'Password must be atleast 8 characters' }),
+    pfp: z.string().url({ message: 'pfp must be a valid URL' }),
+  })
+  .partial();
+
 export type CreateUserInput = z.infer<typeof createUserSchema>;
 export type LoginUserInput = z.infer<typeof loginUserSchema>;
+export type UpdateUserInput = z.infer<typeof updateUserSchema>;
